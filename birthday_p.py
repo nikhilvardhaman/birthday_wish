bfc87e5540dc01dcb91d7eb37583d8c92aca0638
nikhilvaardhaman <nikhilvardhaman@gmail.com>
2025-04-10 15:37:14 +0530

    w

--- birthday_p.py
+++ birthday_p.py
@@ -4,7 +4,7 @@ import os
 
 # Set your OpenAI API key
 
-client = OpenAI(api_key=st.secrets("OPENAI_API_KEY"))
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 st.set_page_config(page_title="🎂 Birthday Wishes for Pinku", page_icon="💖")
 
